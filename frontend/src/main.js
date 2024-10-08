b1899b4de87aff88dbc273f0022a7ae202f7b18e
Adam Kurek <kurekadam314@gmail.com>
2024-10-08 13:19:42 +0200

    Initial commit frontend

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,8 @@
-import './assets/main.css'
-
+import Button from "primevue/button"
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import Aura from '@primevue/themes/aura';
+import PrimeVue from 'primevue/config';
 import App from './App.vue'
 import router from './router'
 
@@ -10,5 +10,14 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+});
+
+
+app.component('Button', Button);
+
 
 app.mount('#app')
